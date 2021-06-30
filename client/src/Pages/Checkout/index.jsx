975a2262050c89c456fe94d5941a4b4cb844d30b
LeoFonsecaP <leonardofonsecap@usp.br>
2021-06-30 17:33:29 -0300

    Checkout form styled

--- client/src/Pages/Checkout/index.jsx
+++ client/src/Pages/Checkout/index.jsx
@@ -11,48 +11,56 @@ function Checkout() {
     <div className="container">
         {!finishOrder ? (
           <>
-          <form>
-              <input type="text" id="name" placeholder="Nome Completo"></input>
-              <input type="text" id="email" name="email" placeholder="email@exemplo.com"></input>
-              <input type="text" id="adress" placeholder="Endereço"></input>
-              <input type="text" id="city" placeholder="Cidade"></input>
-              <select id="state">
-                  <option value="AC">Acre</option>
-                  <option value="AL">Alagoas</option>
-                  <option value="AP">Amapá</option>
-                  <option value="AM">Amazonas</option>
-                  <option value="BA">Bahia</option>
-                  <option value="CE">Ceará</option>
-                  <option value="DF">Distrito Federal</option>
-                  <option value="ES">Espírito Santo</option>
-                  <option value="GO">Goiás</option>
-                  <option value="MA">Maranhão</option>
-                  <option value="MT">Mato Grosso</option>
-                  <option value="MS">Mato Grosso do Sul</option>
-                  <option value="MG">Minas Gerais</option>
-                  <option value="PA">Pará</option>
-                  <option value="PB">Paraíba</option>
-                  <option value="PR">Paraná</option>
-                  <option value="PE">Pernambuco</option>
-                  <option value="PI">Piauí</option>
-                  <option value="RJ">Rio de Janeiro</option>
-                  <option value="RN">Rio Grande do Norte</option>
-                  <option value="RS">Rio Grande do Sul</option>
-                  <option value="RO">Rondônia</option>
-                  <option value="RR">Roraima</option>
-                  <option value="SC">Santa Catarina</option>
-                  <option value="SP">São Paulo</option>
-                  <option value="SE">Sergipe</option>
-                  <option value="TO">Tocantins</option>
-                  <option value="EX">Estrangeiro</option>
-              </select>
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <input type="text" id="zip" name="zip" placeholder="CEP"></input>
-              <input type="tel" id="phone" placeholder=" ()00000-0000"></input>
+          <div className="form-2-sides">
+            <div className="form-left">
+              <h2>Informações</h2>
+            <form className = "checkout">
+                  <input type="text" id="name"  placeholder="Nome Completo"></input>
+                  <input type="text" id="email" name="email" placeholder="email@exemplo.com"></input>
+                  <input type="text" id="adress" placeholder="Endereço"></input>
+                  <input type="text" id="city" className="mip" placeholder="Cidade"></input>
+                  <select className="sel">
+                    <option value="" disabled selected hidden>Estado</option>
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                    <option value="EX">Estrangeiro</option>
+                  </select>
+                  <input type="text" id="zip" name="zip" placeholder="CEP" className="sip"></input>
+                </form>
+            </div>
 
-              <input type="text" id="cname" name="cardname" placeholder="Card name"></input>
-              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"></input>
-              <select id="state">
+            <div className="form-right">
+            <form className="checkout">
+              <h2>Pagamento</h2>
+                <input type="text" id="cname" name="cardname" placeholder="Card name"></input>
+                <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"></input>
+                <select className="sel">
+                  <option value="" disabled selected hidden>Mês</option>
                   <option value="1">Janeiro</option>
                   <option value="2">Fevereiro</option>
                   <option value="3">Março</option>
@@ -65,14 +73,17 @@ function Checkout() {
                   <option value="10">Outubro</option>
                   <option value="11">Novembro</option>
                   <option value="12">Dezembro</option>
-              </select>
-              <input type="text" id="expyear" name="expyear" placeholder="Ano"></input>
-              <input type="text" id="cvv" name="cvv" placeholder="CVV"></input>
-              <label>
-                <input type="checkbox" checked="checked" name="sameadr"></input>
-                &nbsp;Shipping address same as billing
-              </label>
-          </form>
+                </select>
+                <input type="text" id="expyear" className="sip" name="expyear" placeholder="Ano"></input>
+                <input type="text" id="cvv" className="sip" name="cvv" placeholder="CVV"></input>
+                <br></br>
+                <label>
+                  <input type="checkbox" checked="checked" name="sameadr"></input>
+                  &nbsp;Shipping address same as billing
+                </label>
+            </form>
+            </div>
+          </div>
           <div className = "text-right">
               <button id="buyBtn" onClick={() => setFinishOrder(true)}>
                 Finalizar
